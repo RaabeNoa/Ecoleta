503a2a0c4215fcd847d96d7d465d4fa14a49805c
raabe <raabe@explicae.com.br>
2020-06-06 22:16:40 -0300

    add estilos

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import './App.css';
 
-function App() {
+//JSX: Sintaxe de HTML dentro do Javascript
+/** Conceito de imutabilidade React: O valor das constantes é atribuido sem alterar diretamente o valor original dela */
+ /* formato do vetor useState() -> [valor do estado, função p atualizar o valor do estado] const [counter, setCounter] = useState(0) */
+
+ import Routes from './routes';
+
+ function App() {                            
   return (
-    <h1>Hello World</h1>
+      <Routes />
   );
 }
 
